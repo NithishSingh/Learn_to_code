4566e1ea448adaadad30fbecb35fb834ebbdf3b4
Nithish Singh <>
2022-09-23 08:06:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+print("Hello Replit")
+print("Just useing More Lines")
+print("""Anything that starts with 3 quotes, can ends in 3 quotes can span many lines and even contain" within it without freaking anything out! """)
+
+print("What could go Wrong?")
+print("MUSIC+ +")
+print("> Songs")
+print("> Albums")
+print("> Artists")
